ee45f1536015994bc04b3f280acbc3f6a5edd7d1
Ashrit-Yarava <ashdragon95@gmail.com>
2022-08-05 20:35:31 -0400

    Deleted random files.

--- 2D Simulation/GPU/src/mPath/DtableG.py
+++
@@ -1,9 +0,0 @@
-from src.mPath.DtableB import DtableB
-
-
-def DtableG(t, p, rtOff, tau):
-    """
-    Table function for an arbitary time.
-    """
-    tB = t % 2
-    return DtableB(tB + tau, p, rtOff)
--- 2D Simulation/GPU/src/mPath/tableG.py
+++
@@ -1,8 +0,0 @@
-from src.mPath.tableB import tableB
-
-
-def tableG(t, p, rtOff, tau):
-    # Table function for an arbitary time.
-    tB = t % 2
-    y = tableB(tB + tau, p, rtOff)
-    return y
